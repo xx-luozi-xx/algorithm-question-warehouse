c22dc3c4bb851e77015cda5fa78d64c8a51c38c2
xx-luozi-xx <xx_luozi_xx@qq.com>
2024-04-04 13:32:33 +0800

    https://www.matiji.net/exam/brushquestion/25/3181/1DC60EA6DF83A333301CFFE1407FBA59

---
+++ 比赛/百度松果/周赛1/3.cpp
@@ -0,0 +1,18 @@
+// https://www.matiji.net/exam/brushquestion/25/3181/1DC60EA6DF83A333301CFFE1407FBA59
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, n; cin >> a >> n;
+    a %= n;
+    for(int i = 0; i < 1e7; ++i){
+        if(a == 0){
+            cout << "Yes"; 
+            return 0;
+        }
+        a <<= 1; a %= n;
+    }
+    cout << "No";
+}
+//计时器骗分大法
